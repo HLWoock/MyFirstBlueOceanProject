40966b035c6ff21e8289c694bf104cac0017278a
Hermann Woock <Hermann.Woock@Web.de>
2019-03-10 19:01:41 +0100

    initial

---
+++ src/main/java/de/woock/jenkins/MyFirstBlueOceanProjectApplication.java
@@ -0,0 +1,13 @@
+package de.woock.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstBlueOceanProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstBlueOceanProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/woock/jenkins/MyFirstBlueOceanProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package de.woock.jenkins;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyFirstBlueOceanProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
